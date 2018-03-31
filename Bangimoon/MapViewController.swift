aea616ffa834e26d53e9972a5041e11c974ca1d0
mseokim <min101110001@icloud.com>
2018-03-31 17:18:38 +0900

    first

---
+++ Bangimoon/MapViewController.swift
@@ -0,0 +1,36 @@
+//
+//  MapViewController.swift
+//  Bangimoon
+//
+//  Created by 김민서 on 2018. 3. 31..
+//  Copyright © 2018년 김민서. All rights reserved.
+//
+
+import UIKit
+import CoreBluetooth
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
